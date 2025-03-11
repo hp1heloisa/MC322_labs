68f1fc31fe5dd7cbcfa678490e9730eee6659c6f
heloisapimentel <heloisapimentel@ibm.com>
2025-03-11 16:04:10 -0300

    feat: main

--- Main.java
+++ Main.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args){
-    Robo robo1;
-    robo1 = new Robo(null, 0, 0)
+    Scanner scanner = new Scanner(System.in);
+    Ambiente ambiente;
+    Robo[] listaRobos = new Robo[50] ;
+    System.out.println("Quantos robos você quer?");
+    int quantRob = scanner.nextInt();
+    
+    for (int i=0; i<quantRob; i++){
+      System.out.println("Qual o nome do robo " + (i+1) + "?");
+      String nome = scanner.nextLine();
+      listaRobos[i] = new Robo(nome,i,i);
+    }
+    // robo1 = new Robo("robo1", 0, 0);
+    // robo2 = new Robo("robo2", 0, 0);
+    ambiente = new Ambiente(10, 10);
+    listaRobos[0].exibirNome();
+    listaRobos[0].exibirPosicao();
+    listaRobos[0].mover(11, 0);
+    listaRobos[0].exibirPosicao();
+    System.out.println(ambiente.dentroDosLimites(listaRobos[0].posicaoX(), listaRobos[0].posicaoY()));
   }
 }
--- Robo.java
+++ Robo.java
@@ -14,4 +14,13 @@ class Robo {
   public void exibirPosicao(){
     System.out.println("Posição: (" + posicaoX + ", " + posicaoY + ")");
   }
+  public void exibirNome(){
+    System.out.println("O nome do seu robo é: " + nome);
+  }
+  public int posicaoX(){
+    return posicaoX;
+  }
+  public int posicaoY(){
+    return posicaoY;
+  }
 }
